7a47404b0db1b7c3b953bc68ddf9ff6029123c73
gumulaola <fx96@stu.pku.edu.cn>
2022-10-22 00:45:51 +0800

    add outstanding log

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 function sendRequests(urls, max, callback) {
   let curNum = 0;
   let result = [];
+  let outstanding = new Set();
   let total = urls.length;
 
   let sendOneRequest = (url) => {
     return new Promise((resolve, reject) => {
       let period = Math.random() * 3000;
       setTimeout(() => {
-        console.log(url + " take " + period + ".");
+        console.log(url + " takes " + period + "and is finished.");
         resolve(url);
       }, period);
     });
@@ -17,11 +18,17 @@ function sendRequests(urls, max, callback) {
     while (curNum < max && urlArr.length > 0) {
       curNum++;
       let urlToSend = urlArr.shift();
+
+      outstanding.add(urlToSend);
+      console.log("Outstanding urls: " + Array.from(outstanding).toString());
       console.log(urlToSend + " is started.");
+
       sendOneRequest(urlToSend).then((res) => {
-        console.log(res + " is finished.");
         result.push(res);
         curNum--;
+        outstanding.delete(res);
+        // console.log("Outstanding urls: " + Array.from(outstanding).toString());
+
         send(urlArr);
         result.length === total && callback();
       });
