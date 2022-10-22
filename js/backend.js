ab65e4c473a6b01b494550fe6a69add5a194d008
gumulaola <fx96@stu.pku.edu.cn>
2022-10-22 14:35:02 +0800

    node-fetch test

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,12 @@ import fetch from "node-fetch";
 
 fetch("https://www.baidu.com")
   .then((res) => {
-    return res.json();
+    return res.text();
   })
-  .then((res) => console.log(res));
+  .then((res) => {
+    console.log(res.slice(0, 300));
+  });
+
+let fetchRes = await fetch("https://www.baidu.com");
+let text = await fetchRes.text();
+console.log(text.slice(0, 300));
