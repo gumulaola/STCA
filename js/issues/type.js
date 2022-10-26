3d43c945ae8bc3ca647611aa5efea84fec71e07a
gumulaola <fx96@stu.pku.edu.cn>
2022-10-26 16:10:25 +0800

    update type

--- js/issues/type.js
+++ js/issues/type.js
@@ -1,5 +1,14 @@
-let arr = [1, 2, 3];
-let a = typeof arr;
+// js 6种基本数据类型 number string boolean null undefined symbol
 
-console.log(a);
+let arr = [1, 2, 3];
+let fn = function () {
+  console.log(1);
+};
+let str = "hello";
+
+console.log(typeof arr);
+console.log(typeof fn);
+console.log(typeof str);
+
 console.log(Object.prototype.toString.call(arr));
+console.log(Object.prototype.toString.call(fn));
