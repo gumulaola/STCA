1930296c23b9945429edf6eff84b4d43c9c8c081
gumulaola <fx96@stu.pku.edu.cn>
2022-11-20 17:44:27 +0800

    add cardpay

--- flexport/CardPay.py
+++ flexport/CardPay.py
@@ -1,3 +1,18 @@
 class CardPay:
-    def __init__(self):
-        self.kind = 0
+    def __init__(self, A, B, C):
+        self.balance = [A, B, C]
+
+    def can(self, arr):
+        for i in range(arr):
+            if arr[i] > self.balance[i]:
+                return False
+
+        return True
+
+    def buy(self, arr):
+        if not self.can(arr):
+            return -1
+        else:
+            for i in range(arr):
+                self.balance[i] -= self.arr[i]
+            return self.balance
